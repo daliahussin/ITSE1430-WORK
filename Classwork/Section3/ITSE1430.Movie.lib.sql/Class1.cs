5c299ff7533f051cb92dbd5d8c666f6c40938821
dalia.hussin <dalia.hussin@my.tccd.edu>
2018-11-12 18:00:58 -0600

    sec3

---
+++ Classwork/Section3/ITSE1430.Movie.lib.sql/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSE1430.Movie.lib.Sql
+{
+    public class SqlMovieDatabase:MovieDatabase
+    {
+
+    }
+    {
+    }
+}
--- Classwork/Section3/ITSE1430.MovieLib/MovieDatabase.cs
+++ Classwork/Section3/ITSE1430.MovieLib/MovieDatabase.cs
@@ -1,70 +1,140 @@
-﻿using System;
+﻿*
+ * ITSE1430
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ITSE1430.MovieLib
+namespace Itse1430.MovieLib.Memory
 {
     /// <summary>Manages a set of movies.</summary>
-    public abstract class MovieDatabase
+    public class MemoryMovieDatabase : MovieDatabase
     {
         /// <summary>Adds a movie to the database.</summary>
         /// <param name="movie">The movie to add.</param>
-        public void Add(Movie movie)
+        protected override void AddCore(Movie movie)
         {
-            //TODO: Validate
-            if (movie == null)
-                return;
-
-            AddCore(movie);
+            //throw new Exception("Failed");
+            _items.Add(movie);
         }
-
-        protected abstract void AddCore(Movie movie);
+        //Using an expression body for single statement methods
+        //{
+        //    _items.Add(movie);
+        //    //var index = FindNextFreeIndex();
+        //    //if (index >= 0)
+        //    //    _movies[index] = movie;
+        //}
 
         /// <summary>Gets all the movies.</summary>
         /// <returns>The list of movies.</returns>
-        public Movie[] GetAll()
+        protected override IEnumerable<Movie> GetAllCore()
         {
-            return GetAllCore();
+            //var i = _items.ToArray();
+            //return _items;
+
+            //Use LINQ to clone movies
+            return from item in _items
+                       //where 
+                   select new Movie()
+                   {
+                       Name = item.Name,
+                       Description = item.Description,
+                       ReleaseYear = item.ReleaseYear,
+                       RunLength = item.RunLength,
+                       IsOwned = item.IsOwned
+                   };
+
+            //return _items.Select(item => new Movie() {
+            //    Name = item.Name,
+            //    Description = item.Description,
+            //    ReleaseYear = item.ReleaseYear,
+            //    RunLength = item.RunLength,
+            //    IsOwned = item.IsOwned
+            //});
+
+            //foreach (var item in _items)
+            //    yield return new Movie() {
+            //        Name = item.Name,
+            //        Description = item.Description,
+            //        ReleaseYear = item.ReleaseYear,
+            //        RunLength = item.RunLength,
+            //        IsOwned = item.IsOwned
+            //    };
+            ////How many movies do we have
+            //var count = _items.Count;
+
+            //var temp = new Movie[count];
+            //var index = 0;
+            //foreach (var movie in _items)
+            //{
+            //    temp[index++] = movie;
+            //};
+
+            //return temp;            
         }
 
-        protected abstract Movie[] GetAllCore();
+        //TODO: Temporary method to clone a movie
+        //private Movie Clone( Movie item )
+        //{
+        //    return new Movie() {
+        //        Name = item.Name,
+        //        Description = item.Description,
+        //        ReleaseYear = item.ReleaseYear,
+        //        RunLength = item.RunLength,
+        //        IsOwned = item.IsOwned
+        //    };
+        //}
 
         /// <summary>Edits an existing movie.</summary>
         /// <param name="name">The movie to edit.</param>
         /// <param name="movie">The new movie.</param>
-        public void Edit(string name, Movie movie)
+        protected override void EditCore(Movie oldMovie, Movie newMovie)
         {
-            //TODO: Validate
-            if (String.IsNullOrEmpty(name))
-                return;
-            if (movie == null)
-                return;
-
             //Find movie by name
-            var existing = FindByName(name);
-            if (existing == null)
-                return;
+            _items.Remove(oldMovie);
 
-            EditCore(existing, movie);
+            //Replace it
+            _items.Add(newMovie);
         }
 
-        protected abstract Movie FindByName(string name);
-        protected abstract void EditCore(Movie oldMovie, Movie newMovie);
+        /// <summary>Finds a movie by its name.</summary>
+        /// <param name="name">The name of the movie.</param>
+        /// <returns>The movie, if any.</returns>
+        protected override Movie FindByName(string name)
+        {
+            //Example with multiple type parameters
+            //var pairs = new Dictionary<string, Movie>();
+            //for (var index = 0; index < _movies.Length; ++index)
+            //foreach (var movie in _items)
+            //{
+            //    //if (String.Compare(name, _movies[index]?.Name, true) == 0)
+            //    if (String.Compare(name, movie.Name, true) == 0)
+            //        return movie;
+            //};
+            //Use extension methods to get first item
+            //return _items.Where(IsName).FirstOrDefault();
+            //return _items.FirstOrDefault(m => String.Compare(name, m.Name, true) == 0);
+
+            return (from m in _items
+                    where String.Compare(name, m.Name, true) == 0
+                    select m).FirstOrDefault();
+
+            //return null;
+        }
 
         /// <summary>Removes a movie.</summary>
         /// <param name="name">The movie to remove.</param>
-        public void Remove(string name)
+        protected override void RemoveCore(string name)
         {
-            //TODO: Validate
-            if (String.IsNullOrEmpty(name))
-                return;
-
-            RemoveCore(name);
+            var movie = FindByName(name);
+            if (movie != null)
+                _items.Remove(movie);
         }
-        protected abstract void RemoveCore(string name);
+
+        #region Private Members
+
+        //private Movie[] _movies = new Movie[100];
+        private List<Movie> _items = new List<Movie>();
+        #endregion
     }
 }
-
-
