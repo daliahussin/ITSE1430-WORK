f8b12cacff39af9c2e22623a8fa272667b32f80f
dalia.hussin <dalia.hussin@my.tccd.edu>
2018-11-28 17:48:01 -0600

    Lab 4

---
+++ Classwork/Section3/ITSE1430.Movie.lib.sql/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSE1430.Movie.lib.sql
+{
+    class AssemblyInfo
+    {
+    }
+}
---
+++ Classwork/Section3/Movie.Mvc/Models/MovieModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movie.Mvc.Models
+{
+    public class MovieModel
+    {
+    }
+}
